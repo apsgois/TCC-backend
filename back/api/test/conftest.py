ac7370239a0e766bf34b8076cc8cf2f6cb0e1def
apsgois <apsgois@outlook.com>
2024-05-23 19:43:01 -0300

    Atualizando repo

--- back/api/test/conftest.py
+++
@@ -1,9 +0,0 @@
-from typing import Generator
-from fastapi.testclient import TestClient
-import pytest
-from main import api
-
-@pytest.fixture(scope="function")
-def client() -> Generator:
-    with TestClient(api) as client:
-        yield client
